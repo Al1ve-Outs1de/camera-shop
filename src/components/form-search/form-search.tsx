ef073c3fac6decedf151e42abcecbb27619f2ef2
Gleb <gleborlov20022002@gmail.com>
2024-02-20 15:40:18 +0300

    implemented arrow navigation in search list

--- src/components/form-search/form-search.tsx
+++ src/components/form-search/form-search.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { camerasApi } from '../../store/camerasApi';
 import { useAppSelector } from '../../hooks';
@@ -7,11 +7,36 @@ import { AppRoute } from '../../consts';
 
 export default function SearchInputComponent() {
   const [searchValue, setSearchValue] = useState('');
+  const listItemsRef = useRef<HTMLAnchorElement[] | null[]>([]);
+  const listRef = useRef<HTMLUListElement | null>(null);
 
   const { data: products = [] } = useAppSelector(camerasApi.endpoints.getProducts.select());
 
   const searchRegExp = new RegExp(searchValue, 'i');
   const searchedProducts = products.filter((product) => searchRegExp.test(product.name));
+
+  const handleArrowKeydown = useCallback((evt: KeyboardEvent) => {
+    if (evt.key !== 'Tab' && evt.key !== 'Enter') {
+      evt.preventDefault();
+    }
+
+    const itemValue = (evt.target as HTMLAnchorElement)?.textContent;
+    const foundIndex = listItemsRef.current.findIndex((item) => item?.textContent === itemValue);
+
+    if (evt.key === 'ArrowDown' && foundIndex !== -1) {
+      listItemsRef.current[foundIndex + 1]?.focus();
+    }
+
+    if (evt.key === 'ArrowUp' && foundIndex) {
+      listItemsRef.current[foundIndex - 1]?.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.addEventListener('keydown', handleArrowKeydown);
+    }
+  }, [handleArrowKeydown, searchValue]);
 
   return (
     <div className={classNames('form-search', { 'list-opened': searchValue })}>
@@ -35,10 +60,19 @@ export default function SearchInputComponent() {
           />
         </label>
         {(searchValue.length >= 3 && !!searchedProducts.length) &&
-          <ul className="form-search__select-list scroller" data-testid='search-list'>
-            {searchedProducts.map((product) => (
+          <ul className="form-search__select-list scroller" data-testid='search-list' ref={listRef}>
+            {searchedProducts.map((product, index) => (
               <li className="form-search__select-item" tabIndex={-1} key={product.id}>
-                <Link style={{ color: 'inherit' }} to={`${AppRoute.Product}/${product.id}`} onClick={() => setSearchValue('')}>{product.name}</Link>
+                <Link
+                  style={{ color: 'inherit' }}
+                  to={`${AppRoute.Product}/${product.id}`}
+                  onClick={() => setSearchValue('')}
+                  ref={(linkElement) => {
+                    listItemsRef.current[index] = linkElement;
+                  }}
+                >
+                  {product.name}
+                </Link>
               </li>
             ))}
           </ul>}
